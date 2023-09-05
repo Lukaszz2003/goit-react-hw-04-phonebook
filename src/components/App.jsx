dcc3b32bab3f5fdbadf2a1d1e708f33403ba605f
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-09-05 20:06:48 +0200

    one

---
+++ src/components/App.jsx
@@ -0,0 +1,73 @@
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import s from './App.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  handleSubmitForm = obj => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, { ...obj }],
+    }));
+  };
+
+  handleChangeInput = e => {
+    const input = e.target;
+
+    this.setState({ [input.name]: input.value });
+  };
+
+  handleDeleteContacts = id => {
+    this.setState({ contacts: this.state.contacts.filter(el => el.id !== id) });
+  };
+
+  componentDidMount() {
+    const data = localStorage.getItem('contacts');
+    const parseData = JSON.parse(data);
+
+    if (parseData) {
+      return this.setState({ contacts: [...parseData] });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      const data = JSON.stringify(contacts);
+      localStorage.setItem('contacts', data);
+    }
+  }
+
+  render() {
+    const { contacts, filter } = this.state;
+    return (
+      <div className={s.wrap}>
+        <div className={s.phonebook}>
+          <h1 className={s.title}>Phonebook</h1>
+          <ContactForm
+            contacts={contacts}
+            handleSubmitForm={this.handleSubmitForm}
+          />
+          <Filter
+            title="Find contacts by name"
+            handleChangeInput={this.handleChangeInput}
+          />
+        </div>
+
+        <div className={s.contacts}>
+          <h2 className={s.subTitle}>Contacts: {contacts.length}</h2>
+          <ContactList
+            filter={filter}
+            contacts={contacts}
+            handleDeleteContacts={this.handleDeleteContacts}
+          />
+        </div>
+      </div>
+    );
+  }
+}
